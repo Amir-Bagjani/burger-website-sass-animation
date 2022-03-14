486b5f1696d2bc43b62a2d52bfe0aa024a79f929
amirbagjni <amirbagjani@gmail.com>
2022-03-15 02:28:10 +0330

    initial sass and navbar elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,27 @@
-import "../styles/navbar.scss"
-
+import "../styles/navbar.scss";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="header">
+      <i id="menu-btn" className="fas fa-bars icons"></i>
+      <i id="search-btn" className="fas fa-search icons"></i>
 
-export default Navbar
+      <nav className="navbar">
+          <a href="#home">Home</a>
+          <a href="#menu">Menu</a>
+          <a href="#about">About</a>
+          <span className="space"></span>
+          <a href="#review">Review</a>
+          <a href="#contact">Contact</a>
+          <a href="#blogs">Blogs</a>
+      </nav>
+
+      <a href="#" className="fas fa-shopping-cart icons"></a>
+
+      <a href="#home" className="logo"><img src="images/logo.png" alt="logo" /></a>
+      
+    </header>
+  );
+};
+
+export default Navbar;
