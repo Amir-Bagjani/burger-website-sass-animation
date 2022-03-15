db732b59d74a9755294da5731e3180d47bb62a8d
amirbagjni <amirbagjani@gmail.com>
2022-03-15 17:47:51 +0330

    home section added

---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,21 @@
+const HomeSection = () => {
+  return (
+    <section className="home" id="home">
+      <div className="content">
+        <img src="images/burger-baner.png" alt="burger-baner" />
+        <h3>So meaty you'll go crazy</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit impedit
+          eligendi libero, officiis recusandae explicabo soluta provident omnis
+          alias culpa esse, nulla magni voluptatum aperiam, odit laboriosam quam
+          quasi ducimus.
+        </p>
+        <a href="#menu" className="btn">
+          our menu
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default HomeSection;
