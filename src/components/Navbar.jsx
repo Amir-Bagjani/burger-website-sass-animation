721f2e994cee75e20c7036701c076260c8774d76
amirbagjni <amirbagjani@gmail.com>
2022-03-15 03:59:18 +0330

    navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,58 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import "../styles/navbar.scss";
 
 const Navbar = () => {
+  const [open, setOpen] = useState("");
+
+  useEffect(() => {
+    window.addEventListener(`scroll`, () => {
+      setOpen("");
+    });
+
+    return () => window.removeEventListener(`scroll`);
+  }, []);
+
   return (
     <header className="header">
-      <i id="menu-btn" className="fas fa-bars icons"></i>
-      <i id="search-btn" className="fas fa-search icons"></i>
+      <i
+        id="menu-btn"
+        className={open === "menu" ? "fas fa-times icons" : "fas fa-bars icons"}
+        onClick={() => setOpen((prev) => (prev === "menu" ? "" : "menu"))}
+      ></i>
+      <i
+        id="search-btn"
+        className={
+          open === "search" ? "fas fa-times icons" : "fas fa-search icons"
+        }
+        onClick={() => setOpen((prev) => (prev === "search" ? "" : "search"))}
+      ></i>
 
-      <nav className="navbar">
-          <a href="#home">Home</a>
-          <a href="#menu">Menu</a>
-          <a href="#about">About</a>
-          <span className="space"></span>
-          <a href="#review">Review</a>
-          <a href="#contact">Contact</a>
-          <a href="#blogs">Blogs</a>
+      <nav className={open === "menu" ? `navbar active` : `navbar`}>
+        <a href="#home">Home</a>
+        <a href="#menu">Menu</a>
+        <a href="#about">About</a>
+        <span className="space"></span>
+        <a href="#review">Review</a>
+        <a href="#contact">Contact</a>
+        <a href="#blogs">Blogs</a>
       </nav>
 
       <a href="#" className="fas fa-shopping-cart icons"></a>
 
-      <a href="#home" className="logo"><img src="images/logo.png" alt="logo" /></a>
-      
+      <a href="#home" className="logo">
+        <img src="images/logo.png" alt="logo" />
+      </a>
+
+      <form
+        className={open === "search" ? `search-form active` : `search-form`}
+      >
+        <input type="search" placeholder="search here..." id="search-box" />
+        <label
+          htmlFor="search-box"
+          className="fas fa-search icons"
+        ></label>
+      </form>
     </header>
   );
 };
